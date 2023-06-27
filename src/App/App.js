0db62a24088ec2b6d328ac787096243d3bd50bc6
Chrxs <trabajo.ochoachristopher@gmail.com>
2023-06-26 19:03:05 -0500

    Se agrego funcionalidades usando localStorage

--- src/App/App.js
+++ src/App/App.js
@@ -1,32 +1,36 @@
 import React from "react";
 import "./App.css";
-import { TodoItem } from "./components/TodoItem/TodoItem";
-import { TodoCounter } from "./components/TodoCounter/TodoCounter";
-import { TodoList } from "./components/TodoList/TodoList";
-import { TodoSearch } from "./components/TodoSearch/TodoSearch";
-import { CreateTodoButton } from "./components/CreateTodoButton/CreateTodoButton";
-const defaultTodos = [
-  {
-    key: 1,
-    text: "Cortar cebolla Cortar cebolla Cortar cebolla Cortar cebolla Cortar cebolla Cortar cebolla",
-    completed: true,
-  },
-  {
-    key: 2,
-    text: "Cortar tomate Cortar tomate Cortar tomate Cortar tomate Cortar tomate",
-    completed: true,
-  },
-  { key: 3, text: "Cortar cebolla 2", completed: false },
-  { key: "cortarCebolla3", text: "Cortar cebolla 3", completed: false },
-  {
-    key: 4,
-    text: "Cortar tomate Cortar tomate Cortar tomate Cortar tomate Cortar tomate",
-    completed: true,
-  },
-];
+import { TodoItem } from "../components/TodoItem/TodoItem";
+import { TodoCounter } from "../components/TodoCounter/TodoCounter";
+import { TodoList } from "../components/TodoList/TodoList";
+import { TodoSearch } from "../components/TodoSearch/TodoSearch";
+import { CreateTodoButton } from "../components/CreateTodoButton/CreateTodoButton";
+import { useLocalStorage } from "./useLocalStorage";
+
+/*
+// const defaultTodos = [
+//   {
+//     key: 1,
+//     text: "Cortar cebolla Cortar cebolla Cortar cebolla Cortar cebolla Cortar cebolla Cortar cebolla",
+//     completed: true,
+//   },
+//   {
+//     key: 2,
+//     text: "Cortar tomate Cortar tomate Cortar tomate Cortar tomate Cortar tomate",
+//     completed: true,
+//   },
+//   { key: 3, text: "Cortar cebolla 2", completed: false },
+//   { key: "cortarCebolla3", text: "Cortar cebolla 3", completed: false },
+//   {
+//     key: 4,
+//     text: "Cortar tomate Cortar tomate Cortar tomate Cortar tomate Cortar tomate",
+//     completed: true,
+//   },
+// ];
+*/
 
 const App = () => {
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const [todos, saveTodos] = useLocalStorage("TODOS_V1", []);
   const [searchValue, setSearchValue] = React.useState("");
   const searchedTodo = todos.filter((todo) => {
     return todo.text.toLowerCase().includes(searchValue.toLowerCase());
@@ -36,14 +40,14 @@ const App = () => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex((todo) => todo.key === key);
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const removeTodo = (key) => {
     const newTodos = [...todos];
     const todoIndex = newTodos.findIndex((todo) => todo.key === key);
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const completedTodos = todos.filter((todo) => todo.completed).length;
